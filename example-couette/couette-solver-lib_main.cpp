128cc7e9a5ce2740334c7740806686b74653be78
DESKTOP-7DHF2PH\crazyshark12 <mariknorkin@mail.ru>
2023-11-23 22:48:54 +0300

    adding workable but not correctly workin version ( no concentration chsnge, wrong formula for entalp and probably mistakes in calc Fv[energy])

--- example-couette/couette-solver-lib_main.cpp
+++ example-couette/couette-solver-lib_main.cpp
@@ -40,19 +40,24 @@ int main()
 
     MixtureComponent Oxygen2;
     Oxygen2.name = "O2";
-    Oxygen2.density = 1.42897e-3;
+//    Oxygen2.density = 1.42897e-3*0.99;
+    Oxygen2.density = 0.03168;
     Oxygen2.molarMass = 0.0319988;
     Oxygen2.mass = 5.313725014956E-26;
     Oxygen2.epsilonDevK = 1.48281651048e-21/kB;
     Oxygen2.sigma = 3.5155e-10;
+    Oxygen2.numberAtoms = 2;
+    Oxygen2.numberVibrLvl = 15;
+    Oxygen2.omega_e = 1580.19; // см^-1
 
     MixtureComponent Oxygen;
     Oxygen.name = "O";
-    Oxygen.density = 0.03168*0.01; // initial concentration - 0.01
+    Oxygen.density = 0.03168; // initial concentration - 0.01
     Oxygen.molarMass = 0.015999;
     Oxygen.mass = 2.6567628316576e-26;
     Oxygen.epsilonDevK = 1.1045188159999998e-21/kB;
     Oxygen.sigma = 2.75E-10;
+    Oxygen.numberAtoms = 1;
 
     std::vector<MixtureComponent> tmp = {Oxygen2,Oxygen};
     Mixture O2_O(tmp);
@@ -64,17 +69,16 @@ int main()
     /////////////////////
 
     macroParam startParam(mixture);
-    startParam.fractionArray[0] = 0.5;
+    startParam.fractionArray[0] = 0.99;
     startParam.densityArray[0] =  startParam.fractionArray[0] * mixture.components[0].density;
-    if(mixture.NumberOfComponents == 2)
-    {
-        startParam.fractionArray[1] = 0.5;
-        startParam.densityArray[1] =  startParam.fractionArray[1] * mixture.components[1].density;
-    }
+
+    startParam.fractionArray[1] = 0.01;
+    startParam.densityArray[1] =  startParam.fractionArray[1] * mixture.components[1].density;
+
     startParam.density = 0;
     for(int i = 0; i < mixture.NumberOfComponents; i++)
         startParam.density += startParam.densityArray[i];
-    startParam.temp = 999; //140
+    startParam.temp = 800; //140
     startParam.velocity_tau = 0.00001;
 
 
@@ -101,7 +105,7 @@ int main()
     double T2wall = 1000;
     double velocity = 300;
     double h = 1;
-    GodunovSolver solver(mixture,startParam,solParam, SystemOfEquationType::couette2Alt, RiemannSolverType::HLLESolver);
+    GodunovSolver solver(mixture,startParam,solParam, SystemOfEquationType::couette2AltBinary, RiemannSolverType::HLLESolver);
     writer.setDelta_h(h / (solParam.NumCell - 2));
     solver.setWriter(&writer);
     solver.setObserver(&watcher);
