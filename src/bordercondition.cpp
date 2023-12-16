0a143f2ce0da98748bc5ac8d56a71533b7e99358
DESKTOP-7DHF2PH\crazyshark12 <mariknorkin@mail.ru>
2023-12-16 17:01:35 +0300

    minor x2

--- src/bordercondition.cpp
+++ src/bordercondition.cpp
@@ -12,12 +12,12 @@ void BorderConditionCouette::updatePoints(vector<macroParam> &points)
         points[0].pressure = points[1].pressure;
     else
         points[0].density = points[1].density;
-    points[0].densityArray =points[1].densityArray;
-    points[0].fractionArray =points[1].fractionArray;
-    points[0].velocity_tau = -points[1].velocity_tau + 2.* down_velocity;
-    points[0].velocity_normal = -points[1].velocity_normal;
+    points[0].densityArray = points[1].densityArray;
+    points[0].fractionArray = points[1].fractionArray;
+    points[0].velocity_tau = - points[1].velocity_tau + 2.* down_velocity;
+    points[0].velocity_normal = - points[1].velocity_normal;
     points[0].velocity = sqrt(pow(fabs(points[0].velocity_tau),2) + pow(fabs(points[0].velocity_normal),2));
-    points[0].temp = -points[1].temp +  2.* down_temp;
+    points[0].temp = - points[1].temp +  2.* down_temp;
     // дополнительные рассчитываемые величины
     if(!presEq)
         points[0].pressure = points[0].density / mixture.molarMass(points[0].fractionArray) * UniversalGasConstant * points[0].temp;
@@ -33,10 +33,10 @@ void BorderConditionCouette::updatePoints(vector<macroParam> &points)
         points[N-1].density = points[N-2].density;
     points[N-1].densityArray = points[N-2].densityArray;
     points[N-1].fractionArray = points[N-2].fractionArray;
-    points[N-1].velocity_tau = -points[N-2].velocity_tau + 2.* up_velocity;
-    points[N-1].velocity_normal = -points[N-2].velocity_normal;
+    points[N-1].velocity_tau = - points[N-2].velocity_tau + 2.* up_velocity;
+    points[N-1].velocity_normal = - points[N-2].velocity_normal;
     points[N-1].velocity = sqrt(pow(points[N-2].velocity_tau,2) + pow(points[N-1].velocity_normal,2));
-    points[N-1].temp = -points[N-2].temp +  2.* up_temp;
+    points[N-1].temp = - points[N-2].temp +  2.* up_temp;
     // дополнительные рассчитываемые величины
     auto y_c = points[N - 1].fractionArray;
     if(!presEq)
