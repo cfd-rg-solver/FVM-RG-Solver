887c2ce8263176fa0396b8c863690e9f23814e47
ZarinaMax <zarinaimaksudova@gmail.com>
2024-02-09 02:12:25 +0300

    overall idea, python solver doesn't work yet

--- example-shockwave/nonlinear_border_system_solver.py
+++ example-shockwave/nonlinear_border_system_solver.py
@@ -9,8 +9,8 @@ def solver(left_params):
     def func(x):
         return [
             x[0] * x[1] - v_0 * rho_0,
-            x[1] * x[0]^2 + x[2] - rho_0 * v_0^2 - p_0,
-            x[1] * x[0] * (x[3] + x[0]^2/2 + x[2]/x[1]) - rho_0 * v_0 * (E_0 + v_0^2/2 + p_0/rho_0)
+            x[1] * np.pow(x[0],2) + x[2] - rho_0 * np.pow(v_0,2) - p_0,
+            x[1] * x[0] * (x[3] + np.pow(x[0],2)/2 + x[2]/x[1]) - rho_0 * v_0 * (E_0 + np.pow(v_0,2)/2 + p_0/rho_0)
             ]
 
     ans = fsolve(func, [1, 1, 1, 1])
